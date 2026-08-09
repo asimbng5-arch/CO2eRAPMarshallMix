3191f54ec279da378b592794a276d6473a240307
asimbng5-arch <asimbng5@gmail.com>
2026-08-09 19:06:58 -0400

    Update MS+MF+CO2e-app.py

--- MS+MF+CO2e-app.py
+++ MS+MF+CO2e-app.py
@@ -171,11 +171,10 @@ if other_cols:
         val_display = f"{val:.2f}" if isinstance(val, (int, float, np.floating, np.integer)) else val
         st.markdown(f"**{col}:** {val_display}")
 
+# === MS ===
 st.write('---')
 ngb_model_MS = pickle.load(open('ngb_model_MS.pkl', 'rb'))
-ngb_model_MF = pickle.load(open('ngb_model_MF.pkl', 'rb'))
-ngb_model_CO2e = pickle.load(open('ngb_model_CO2e.pkl', 'rb'))
-preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor_MS.pkl', 'rb'))
 X_input_processed = preprocessor.transform(df_input)
 
 dist = ngb_model1.pred_dist(X_input_processed)
@@ -242,7 +241,7 @@ st.write('---')
 # === MF ===
 
 ngb_model2 = pickle.load(open('ngb_model_MF.pkl', 'rb'))
-preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor_MF.pkl', 'rb'))
 X_input_processed = preprocessor.transform(df_input)
 
 dist = ngb_model2.pred_dist(X_input_processed)
@@ -307,7 +306,7 @@ st.download_button(
 # === CO2e ===
 
 ngb_model2 = pickle.load(open('ngb_model_CO2e.pkl', 'rb'))
-preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor_CO2e.pkl', 'rb'))
 X_input_processed = preprocessor.transform(df_input)
 
 dist = ngb_model2.pred_dist(X_input_processed)
